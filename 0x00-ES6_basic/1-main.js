d68d7a1c3e349039581def427bee8e61c5385154
ScottShadow <rudaseswascottmc@gmail.com>
2024-06-25 11:16:53 +0200

    arrow fix

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,4 +1,5 @@
-import taskBlock from './1-block-scoped.js';
-
+import taskBlock from './1-block-scoped';
+/* eslint-disable no-console */
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+/* eslint-enable no-console */
