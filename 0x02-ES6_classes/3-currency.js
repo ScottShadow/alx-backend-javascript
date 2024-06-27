34aaf24ed96ea1bb0e9376099ea3cda895d31736
ScottShadow <rudaseswascottmc@gmail.com>
2024-06-27 16:34:45 +0200

    updated 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,10 +1,30 @@
 export default class Currency {
-  constructor(code, string) {
+  constructor(code, name) {
     this.code = code;
-    this.string = string;
+    this.name = name;
   }
 
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
   displayFullCurrency() {
-    return `${this.string} (${this.code})`;
+    return `${this.name} (${this.code})`;
   }
 }
