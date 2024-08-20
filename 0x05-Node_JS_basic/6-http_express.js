1d1d40d46a545b633c34fc3675c4c21c4b47f724
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-20 23:24:21 +0200

    added docu2

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,29 @@
+/**
+ * Creates an Express server that listens on port 1245
+ * and responds to GET requests on the root path with
+ * "Hello Holberton School!".
+ *
+ * @module 6-http_express
+ */
+
+const express = require('express');
+const app = express();
+const port = 1245;
+
+/**
+ * GET /
+ * Responds with "Hello Holberton School!".
+ */
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+/**
+ * Starts the Express server listening on the specified port.
+ */
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
+
