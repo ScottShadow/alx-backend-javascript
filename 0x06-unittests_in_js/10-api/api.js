6f3ce92752d468fffd3d90a84b530ce7eb1ad948
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-22 07:53:37 +0200

    updated files

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,42 @@
 #!/usr/bin/node
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const cartId = Number(req.params.id);
+  if (Number.isNaN(cartId)) {
+    res.status(404).send('Not Found');
+  } else {
+    res.status(200).send(`Payment methods for cart ${req.params.id}`);
+  }
+});
+app.get('/available_payments', (req, res) => {
+  const payload = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.status(200).send(payload);
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  if (userName) {
+    res.status(200).send(`Welcome ${userName}`);
+  }
+});
+app.listen(PORT, () => {
+  console.log('API available on localhost port 7865');
+});
+module.exports = app;
