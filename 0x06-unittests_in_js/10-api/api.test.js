950a17416c5f2ba86d7bc10a18f2f3b5e4abb9d5
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-26 06:39:19 +0200

    added mocha to scripts

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const request = require('request');
 const { expect } = require('chai');
 
@@ -21,14 +22,14 @@ describe('API integration test', () => {
   });
 
   it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
-    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
-    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
